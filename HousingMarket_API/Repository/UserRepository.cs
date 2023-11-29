ca8352322e310f507273aae67bbc9e36d32c8c0a
you1424 <151393247+you1424@users.noreply.github.com>
2023-11-29 15:00:13 -0500

    UserRepository.cs
    I haven not put any code here yet.

---
+++ HousingMarket_API/Repository/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HousingMarket_API.Repository
+{
+    public class UserRepository : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
