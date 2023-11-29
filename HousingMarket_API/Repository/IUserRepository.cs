37a498725d9a93e30412eeb60d7e0c1eb1d29891
you1424 <151393247+you1424@users.noreply.github.com>
2023-11-29 14:58:38 -0500

    IUserRepository.cs

---
+++ HousingMarket_API/Repository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using HousingMarket_API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingMarket_API.Repository
+{
+    public interface IUserRepository
+    {
+        void Add(UserModel user);
+        IEnumerable<UserModel> GetAll();
+        UserModel GetById(int userId);
+        void Update(UserModel user);
+        void Delete(int userId);
+    }
+}
